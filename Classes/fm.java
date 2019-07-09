5902f65d87a5a50ac11d59f13262204d9d18e8b1
lucasfernandescosta <49734316+lucasfernandescosta@users.noreply.github.com>
2019-07-09 08:52:23 -0300

    Add files via upload

---
+++ Classes/fm.java
@@ -0,0 +1,16 @@
+package com.lucas.interface.Classes;
+
+public class fm {
+	
+	private float frequencia;
+
+	public float getFrequencia(){
+		return frequencia;
+	}
+
+	public void setFrequencia(float frequencia){
+		this.frequencia = frequencia;
+	}
+
+
+}
---
+++ Classes/temp.java
@@ -0,0 +1,16 @@
+package com.lucas.interface.Classes;
+
+public class temp {
+	private float graus;
+
+	public float getGraus(){
+		return graus;
+	}
+	
+	public void setGraus(float graus){
+		this.graus = graus;
+	}
+
+
+
+}
